675036b2721f70e476245a1d1bd807e0d47b449f
codingriobit <leonardo.sanchez@riobit.co>
2022-01-25 16:20:04 -0500

    quitando comentarios console.log

--- src/js/utilportal.js
+++ src/js/utilportal.js
@@ -8,18 +8,9 @@ Array.prototype.forEach.call(uniJSIdiomaTrigger, function(e){
         var portalesMenuWrapper = $(".js-portales-menu-wrapper");
         var portalesBtn = $(".js-portales-trigger");
 
-  
-  console.log("antes del if para entrar a preguntar si esta abierto");
-  
   if ($(portalesBtn).attr("data-click-state") == 1) {
       
-      console.log("dentro del if antes de setear ");
-
       $(portalesBtn).attr("data-click-state", 0);
-
-      console.log("despues  del if supuestamente seteado ");
-
-
       portalesMenuWrapper.css("width", "0");
 
     }
